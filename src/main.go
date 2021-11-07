8f3f48a1ffa9e1343f77892ecbbed53969c7c9c4
aselimkaya <aselimkaya@outlook.com>
2021-11-07 14:11:10 +0300

    simple simulator template implemented

--- src/main.go
+++ src/main.go
@@ -5,10 +5,14 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/aselimkaya/nbasimulator/src/simulator"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	s := simulator.New()
+	s.Run()
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
