9d5bb8b4626643f5773ac148799cda75e22791f4
aselimkaya <aselimkaya@outlook.com>
2021-11-08 02:19:59 +0300

    database and server are running concurrently at start up

--- src/main.go
+++ src/main.go
@@ -4,18 +4,32 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"sync"
 
 	"github.com/aselimkaya/nbasimulator/src/simulator"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	s := simulator.New()
-	s.Run()
+	wg := sync.WaitGroup{}
+	wg.Add(2)
 
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", os.Getenv("SERVER_PORT"))))
+	go func() {
+		defer wg.Done()
+		s := simulator.New()
+		s.InitFromAPI()
+		s.Run()
+	}()
+
+	go func() {
+		defer wg.Done()
+
+		e := echo.New()
+		e.GET("/", func(c echo.Context) error {
+			return c.String(http.StatusOK, "Hello, World!")
+		})
+		e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", os.Getenv("SERVER_PORT"))))
+	}()
+
+	wg.Wait()
 }
